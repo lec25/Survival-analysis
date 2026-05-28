50f84b6f7dd6b0aaef00bdbd814aae3aaaf35be7
yz-apr <zhouyi0426@hotmail.com>
2026-05-28 19:21:46 +0900

    update

---
+++ KMdata.R
@@ -0,0 +1,21 @@
+##
+## Title:    Data in Survival analysis
+## Creator:  Yi
+## Date:     2026-05-28
+##
+## GitHub:   https://github.com/jun-yan/KMsurv
+##---------------------------------------------------------------------
+
+## Install the package for the first time
+# install.packages(KMsurv)
+
+## load the contents of the package
+library(KMsurv)
+
+## show the document of package
+library(help = KMsurv)
+
+## load one dataset
+data(aids)
+aids
+
